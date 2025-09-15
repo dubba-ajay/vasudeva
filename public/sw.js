ef02939409866f8aee2d778cbcc45eb21862bf1b
dubba-ajay <dubbaajay95@gmail.com>
2025-09-15 11:13:00 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'bliss-cache-v1';
+const CORE_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      const fetchPromise = fetch(req).then((network) => {
+        const copy = network.clone();
+        caches.open(CACHE).then((cache) => cache.put(req, copy));
+        return network;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
